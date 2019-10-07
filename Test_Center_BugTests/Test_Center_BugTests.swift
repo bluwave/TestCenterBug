68dcef9c6a21f2cbb11a5be1702e9f99a3ef6be6
Garrett Richards <garrettrichards@gmail.com>
2019-10-07 15:39:48 -0600

    initial commit

---
+++ Test_Center_BugTests/Test_Center_BugTests.swift
@@ -0,0 +1,31 @@
+//
+//  Test_Center_BugTests.swift
+//  Test_Center_BugTests
+//
+//  Created by Garrett Richards on 10/7/19.
+//  Copyright © 2019 Acme. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Center_Bug
+
+class Test_Center_BugTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        let foo = 1
+        XCTAssertEqual(foo, 2)
+    }
+
+    func testExample2() {
+        let foo = 2
+        XCTAssertEqual(foo, 2)
+    }
+}
